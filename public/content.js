131ddb0868ad8aa43f0c5d88fb96e5ac3b200a50
Desarso <malek.gabriel33@gmail.com>
2022-06-14 00:27:48 -0700

    Update content.js

--- public/content.js
+++ public/content.js
@@ -1,16 +1,19 @@
-
-
-
 chrome.runtime.onMessage.addListener((msg ,sender, response)=>{
     console.log(msg);
     console.log(response);
     // check every 100ms
+    //I don't know why it chekcs every 100ms or how to change that.
     
 })
+
+
+
+//oh this functions is basically trying to remove the toast warning pop up very 100ms.
 
 chrome.runtime.sendMessage({status: "?"},function(response){
     console.log(response)
     if (response.status == "on"){
+        //this interval sets the 100ms timing.
         var existCondition = setInterval(function() {
             if (document.querySelector('#toast-container')) {
                console.log("Exists!");
@@ -21,5 +24,7 @@ chrome.runtime.sendMessage({status: "?"},function(response){
             //    doTheRestOfTheStuff(parameters);
             }
            }, 100);
+
+           //this 100 probably has something to do with the delay.
     }
   })
